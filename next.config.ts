263d763144ddb2ce026a5729b4afb192e1d8f57d
zxck1l <172283809+zxck1l@users.noreply.github.com>
2024-12-04 15:45:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+export default {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com'
+      }
+    ]
+  }
+};
